1602ef8d3d04a52e0c3bc7facba470800c47d736
Steven Valdez <dvorak42@mit.edu>
2012-03-09 15:41:41 -0500

    Updated WebRenderer

--- WebRenderer.py
+++ WebRenderer.py
@@ -6,8 +6,21 @@ from PyQt4.QtGui import *
 from PyQt4.QtWebKit import *
 from PyQt4.QtNetwork import *
 
+DEBUG=True
+'''Currently, you should do:
+wr = WebRenderer()
+wr.render("URL")
+--Wait until wr.loading == False
+wr.image should contain the image you need.
+
+'''
 class WebRenderer:
 	def loadHandler(self, result):
+		self.page.setViewportSize(self.page.mainFrame().contentsSize())
+		self.image = QImage(self.page.viewportSize() + QSize(10,10), QImage.Format_ARGB32)
+		painter = QPainter(self.image)
+		self.page.mainFrame().render(painter)
+		painter.end()
 		self.loading = False
 
 	def __init__(self):
@@ -17,22 +30,10 @@ class WebRenderer:
 		self.window = QMainWindow()
 		self.window.setCentralWidget(self.view)
 		self.page.connect(self.page, SIGNAL("loadFinished(bool)"), self.loadHandler)
+		self.image = None
 		
 	def render(self, url):
 		self.loading = True
 
 		mf = self.page.mainFrame()
 		mf.load(QUrl(url))
-
-		while self.loading:
-			while QApplication.hasPendingEvents():
-				QCoreApplication.processEvents()
-
-		image = QImage(self.page.viewportSize(), QImage.Format_ARGB32)
-		painter = QPainter(image)
-		mf.render(painter)
-		painter.end()
-
-		return image
-
-
