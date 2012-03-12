7c73af1658c9073607449a15ce587501e183771c
Steven Valdez <dvorak42@mit.edu>
2012-03-12 15:41:47 -0400

    Minor things with main

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import sys
 import signal
-import WebRenderer
-import WikiData
+from WebRenderer import WebRenderer
+from WikiData import WikiData
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -12,8 +12,28 @@ class WikiGame:
 	def __init__(self):
 		self.app = QApplication(sys.argv)
 		signal.signal(signal.SIGINT, signal.SIG_DFL)
-		self.webrender = WebRenderer()
+		self.wr = WebRenderer()
 		self.wikidb = WikiData()
+
+
+	def show(self):
+		w = QWidget()
+		w.resize(250, 150)
+		w.move(300, 300)
+		w.setWindowTitle('Simple')
+		w.show()
+
+		self.page = QWebPage()
+		self.view = QWebView()
+		self.view.setPage(self.page)
+		self.window = QMainWindow()
+		self.window.setCentralWidget(self.view)
+
+		mf = self.page.mainFrame()
+		mf.load(QUrl("google.com"))
+		self.window.show()
+		sys.exit(self.app.exec_())
 
 if __name__ == '__main__':
 	game = WikiGame()
+	game.show()
