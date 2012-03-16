6bf4ded8900a5a2d9a9e6dd6fecdcd89f4668503
Jordan Moldow <jmoldow@mit.edu>
2012-03-16 01:35:41 -0400

    Adds main menu and instructions

--- Main.py
+++ Main.py
@@ -14,6 +14,8 @@ from PyQt4.QtNetwork import *
 import Image	# PIL module for image manipulation
 office_chromatic_map = Image.open('media/office_background_images/officechromaticmap.png')  # The Image file that contains the chromatic map
 main_panel = None   # Dirty hack to allow us access to the main_panel in the below functions. TODO: Remove dirty hack.
+main_menu = None	# Global variable for accessing the main_menu in the below functions.
+instructions = None # Global variable for accessing the instructions in the below functions.
 office_images = os.listdir('media/office_background_images')	# A list of all of the images in the office_background_images directory
 cursor = None   # Mouse cursor. Needs to be global because of the unfortunate way that the main_panel is implemented. TODO: Remove dirty hack.
 game = Game()   # Global variable that stores the game
@@ -36,13 +38,19 @@ def mainPanelMouseMoveEvent(event):
 		cursor.setShape(Qt.ArrowCursor) # Return to the default cursor
 	main_panel.setCursor(cursor)	# Reassign the cursor to the main_panel
 
+def getPos(event):
+	'''
+Function that returns a tuple of the position of the mouse event.
+	'''
+	pos = event.pos() # the position of the mouse, as a QPoint
+	return (pos.x(), pos.y()) # the position of the mouse, as a 2-tutple of integers
+
 def getColor(event):
 	'''
 Function that returns the Hex string (without the leading 0x) of the color of the chromatic map at the position of the mouse event.
 	'''
 	global office_chromatic_map
-	pos = event.pos()   # the position of the mouse, as a QPoint
-	pos = (pos.x(), pos.y())	# the position of the mouse, as a 2-tutple of integers
+	pos = getPos(event)   # the position of the mouse, as a 2-tutple of integers
 	return ''.join(map(lambda x: ('0'+pyhex(int(x))[2:].upper())[-2:], office_chromatic_map.getpixel(pos)[0:3]))   # The Hex string (without the leading 0x) of the color of the chromatic map at that mouse position
 
 colors = {} # A dictionary that maps labels for the different types of clickables to a tuple of their colors in the chromatic map
@@ -58,15 +66,17 @@ colors['fox'] = ('00FF88',)
 
 def mainPanelMousePressEvent(event):
 # See my comments for mainPanelMouseMoveEvent
-	global main_panel, cursor, game, opened_windows
+	global main_panel, main_menu, cursor, game, opened_windows
 	color = getColor(event)
 	widget = None   # The widget returned by the clickable
 	if color in colors['exit']: # exit lamp
 		sys.exit()
 	elif color in colors['menu']: # main menu
-		game.restart()  # Returning to the menu should restart the game. Later I will add the actual menu functionality.
+		game.restart()  # Returning to the menu should restart the game
+		main_menu.raise_() # hide the menu behind everything else
+
 	elif color in colors['files']:   # files
-		widget = game.showClue(colors['files'].index(color))    # Clicking on a folder returns a clue widget
+		widget = game.showClue(colors['files'].index(color))	# Clicking on a folder returns a clue widget
 	elif color in colors['photos']:
 		widget = game.showClipping(colors['photos'].index(color))   # Clicking on a photo returns a clipping widget
 	elif color in colors['gun']:
@@ -97,9 +107,56 @@ Clear the background image for widget, set the background color as transparent.
 	'''
 	widget.setStyleSheet("QWidget{ background-image: none; background-color: transparent; }")
 
+menu_buttons = {}   # Dictionary that maps labels for the menu buttons to rectangles describing their positions
+menu_buttons['play'] = QRect(QPoint(70,570),QPoint(180,645))
+menu_buttons['instructions'] = QRect(QPoint(220,570),QPoint(485,645))
+menu_buttons['exit'] = QRect(QPoint(525,570),QPoint(640,645))
+
+def mainMenuMouseMoveEvent(event):
+	global menu_buttons, main_menu
+	pos = event.pos()
+	in_button = False
+	for label,rect in menu_buttons.iteritems():
+		if rect.contains(pos):
+			cursor.setShape(Qt.PointingHandCursor)  # Change to the pointing hand cursor
+			in_button = True
+	if not in_button:
+		cursor.setShape(Qt.ArrowCursor) # Return to the default cursor
+	main_menu.setCursor(cursor)
+
+def mainMenuMousePressEvent(event):
+	global menu_buttons, main_menu, instructions
+	pos = event.pos()
+	if menu_buttons['play'].contains(pos):
+		main_menu.lower()
+		instructions.lower()
+	elif menu_buttons['instructions'].contains(pos):
+		main_menu.lower()
+		instructions.raise_()
+	elif menu_buttons['exit'].contains(pos):
+		sys.exit()
+
+return_to_menu = QRect(QPoint(570,560),QPoint(685,650))
+
+def instructionsMouseMoveEvent(event):
+	global instructions, return_to_menu
+	pos = event.pos()
+	if return_to_menu.contains(pos):
+		cursor.setShape(Qt.PointingHandCursor)  # Change to the pointing hand cursor
+	else:
+		cursor.setShape(Qt.ArrowCursor) # Return to the default cursor
+	instructions.setCursor(cursor)
+
+def instructionsMousePressEvent(event):
+	global instructions, main_menu, return_to_menu
+	pos = event.pos()
+	if return_to_menu.contains(pos):
+		instructions.lower()
+		main_menu.raise_()
+
 class WikiGame:
 	def __init__(self):
-		global main_panel, cursor   # Dirty hack to allow us to refer to the main_panel, cursor in the above functions. TODO: Remove dirty hack.
+		global main_panel, main_menu, instructions, cursor   # Dirty hack to allow us to refer to the main_panel, cursor in the above functions. TODO: Remove dirty hack.
 		self.app = QApplication(sys.argv)
 		signal.signal(signal.SIGINT, signal.SIG_DFL)
 		self.wr = WebRenderer()
@@ -138,6 +195,26 @@ class WikiGame:
 		self.main_panel.mouseMoveEvent = mainPanelMouseMoveEvent	# Set the above-defined function as a static object method. TODO: Remove dirty hack.
 		self.main_panel.setCursor(cursor)
 		self.main_panel.mousePressEvent = mainPanelMousePressEvent
+
+		# set up the main menu
+		main_menu = self.main_menu = QWidget(self.window)
+		self.main_menu.setGeometry(self.MAIN_PANEL_RECT)
+		setBackgroundImage(self.main_menu, 'media/menu/menu.png')
+		self.main_menu.setMouseTracking(True)
+		self.main_menu.setCursor(cursor)
+		self.main_menu.raise_()
+		self.main_menu.mouseMoveEvent = mainMenuMouseMoveEvent
+		self.main_menu.mousePressEvent = mainMenuMousePressEvent
+
+		# set up the instructions page
+		instructions = self.instructions = QWidget(self.window)
+		self.instructions.setGeometry(self.MAIN_PANEL_RECT)
+		setBackgroundImage(self.instructions, 'media/menu/instructions.png')
+		self.instructions.setMouseTracking(True)
+		self.instructions.setCursor(cursor)
+		self.instructions.lower()
+		self.instructions.mouseMoveEvent = instructionsMouseMoveEvent
+		self.instructions.mousePressEvent = instructionsMousePressEvent
 
 	def show(self):
 		self.page = QWebPage()
